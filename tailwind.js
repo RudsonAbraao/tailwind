1cb4364cb4d01a740924e8a36c1b54e6f65bb22e
jaqueline <jaquelinenepomuceno.2019@gmail.com>
2023-07-11 18:18:46 -0300

    projeto finalizado

--- tailwind.js
+++ tailwind.js
@@ -10,6 +10,19 @@ tailwind.config = {
             },
             fontFamily: {
                 inter: ['Inter', 'sans-serif']
+            },
+            keyframes: {
+                sino_kf:{
+                    '0%, 100%': {
+                        transform: 'rotate(-10deg)'
+                    },
+                    '50%': {
+                        transform: 'rotate(10deg)'
+                    }
+                }
+            },
+            animation: {
+                sino: 'sino_kf 0.31s ease-in-out infinite'
             }
         }
     }
